51a6c725e65181229a0eb8ce1f6bae4c106e0f22
Cezar398 <catarau.ci@gmail.com>
2022-01-06 05:36:15 -0800

    Update

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
-#include <iostream>
+/**< SFML */
 
-using namespace std;
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+/**< END SFML */
+
+#include <bits/stdc++.h> /** BIBLIOTECI STANDARD NECESARE */
+#include <detector.hpp> /**< DETECTOR SISTEM OPERARE URL(https://github.com/TerensTare/detector) */
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Airport Manager Simulator - Catarau Cezar-Iulian");
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+
+    }
 }
